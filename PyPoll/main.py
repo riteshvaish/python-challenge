aad38f3f3045115810d5c06910748dc745b41966
RiteshVaish <ritesh_vaish@yahoo.com>
2019-03-06 14:52:47 -0800

    updated election logic

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,54 +1,58 @@
+import os
 import csv
+import operator
 
-fp=open('./Resources/election_data.csv','r')
-reader=csv.DictReader(fp)
+csvpath = os.path.join(".","Resources","election_data.csv")
+vote_count = 0
+winner_candidate = 'none'
 distData={}
 sorted_distData={}
-totalRows = 0
-for row in reader:
-    #keep track of the total votes casted
-    totalRows = totalRows + 1
-        #if dict element existes the increment the value
-    if row['Candidate'] in distData:
-        thisCount = distData[row['Candidate']]
-        newCount = thisCount + 1
-        distData[row['Candidate']] = newCount
-        #Dict elem does not exists, add now
-    else:
-        distData[row['Candidate']] = 1
-#close the file pointer
-fp.close()
+
+with open (csvpath, newline='') as votefile:
+    
+    csvread = csv.reader(votefile, delimiter=',')
+    # skipo the header
+    next(csvread,None)
+    # read the file row by row
+    for row in csvread:
+        vote_count += 1
+        # Check if the candidate exists in new dictionay
+        if row[2] in distData:
+            thisCount = distData[row[2]]
+            newCount = thisCount + 1
+            distData[row[2]] = newCount
+        # Add the candidate in the dictionay if the name is not present
+        else:
+            distData[row[2]] = 1
 
 #sort the dictionary in descending order. This is the order needed to print in the report
 sorted_distData = sorted(distData.items(), key=lambda x: x[1], reverse=True)
-#print distData
-#print sorted_distData
 
 #Now generate report
 
-print (f"\nElection Results \n------------------------- \nTotal Votes: {str(totalRows)} ")
+print (f"\nElection Results \n------------------------- \nTotal Votes: {str(vote_count)} \n-------------------------")
 
 #read the dictionary and print the report
 idx = 0
 for Winner in sorted_distData:
-    perCent = float((float(Winner[1])/float(totalRows))*100)
+    perCent = float((float(Winner[1])/float(vote_count))*100)
     print (str(Winner[0]) + ": " + str(format(perCent, '.3f')) + "% (" + str(Winner[1]) + ")")
     idx = idx + 1
-	
+
 
 key_max = max(distData.keys(), key=(lambda k: distData[k]))
 print(f"\n------------------------- \n Winner:  {str(key_max)} \n------------------------- \n ")
 
 #write output to a file
 with open("Output.txt", "w") as text_file:
-    print (f"\nElection Results \n------------------------- \nTotal Votes: {str(totalRows)} ",file=text_file)
+    print (f"\nElection Results \n------------------------- \nTotal Votes: {str(vote_count)} \n-------------------------",file=text_file)
     
     idx = 0
     for Winner in sorted_distData:
-        perCent = float((float(Winner[1])/float(totalRows))*100)
+        perCent = float((float(Winner[1])/float(vote_count))*100)
         print (str(Winner[0]) + ": " + str(format(perCent, '.3f')) + "% (" + str(Winner[1]) + ")", file=text_file)
         idx = idx + 1
-
-
-
+    
+    
+    
     print(f"\n------------------------- \n Winner:  {str(key_max)} \n------------------------- \n ", file=text_file)
